3842dce940863384ccee0bf15654547082ea723b
Wiktor <wmatuszak16@gmail.com>
2024-12-26 20:05:06 +0100

    dodanie roli uzytkownikow

--- Euro.Utility/SD.cs
+++ Euro.Utility/SD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace Euro.Utility
 {
     public static class SD
     {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
     }
 }
