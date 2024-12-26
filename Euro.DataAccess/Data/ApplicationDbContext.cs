4903c422c4b64958853360496341a05b18054872
Wiktor <wmatuszak16@gmail.com>
2024-12-26 19:21:31 +0100

    zmiana identity user na aplication user

--- Euro.DataAccess/Data/ApplicationDbContext.cs
+++ Euro.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace Euro.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
 
